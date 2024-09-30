5a6714316c029117d45db650d2b87cf570145679
KventinBox <164005678+KventinBox@users.noreply.github.com>
2024-10-01 00:50:00 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,52 @@
-let button = document.getElementById('cButton');
-let hiddenImage = document.getElementById('hiddenImage');
-button.addEventListener('click', function() {
-    let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-    button.style.backgroundColor = randomColor;
-    hiddenImage.style.display = 'block';
+let button = document.querySelector('.cs-button');
+let abzac = document.getElementById('pe1');
+let abza = document.getElementById('pe2');
+let abz = document.getElementById('pe3');
+let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+let isChanged = false;
+let orcolor = 'black';
+let originalText = abzac.textContent;
+let sbutton = document.querySelector('.css-button');
+let beka = document.getElementById('pe8');
+let images = document.querySelectorAll('.a, .b, .c');
+let bool = true;
 
-    setTimeout(function() {
-        hiddenImage.style.display = 'none';
-    }, 2000);
+images.forEach(image => {
+    image.addEventListener('click', function() {
+        if (bool) {
+            this.classList.toggle('zoom');
+            bool = false;
+        } else {
+            this.classList.toggle('unzoom');
+            bool = true;
+        }
+        
+    });
 });
 
-calculateButton.addEventListener('click', function() {
-    let num1 = parseFloat(document.getElementById('num1').value);
-    let num2 = parseFloat(document.getElementById('num2').value);
-    let operation = document.getElementById('operation').value; 
-    let resultElement = document.getElementById('result');
 
-    resultElement.textContent = ""; 
-
-    
-    if (!isNaN(num1) && !isNaN(num2)) {
-        let result;
-
-        switch (operation) {
-            case '+':
-                result = num1 + num2; 
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 === 0) {
-                    resultElement.textContent = "Тебя забанили в калькуляторе";
-                    document.getElementById('num1').disabled = true; 
-                    document.getElementById('num2').disabled = true; 
-                    document.getElementById('operation').disabled = true; 
-                    return; 
-                }
-                result = num1 / num2; 
-                break;
-            default:
-                resultElement.textContent = "Выбери чо хочеш"; 
-                return; 
-        }
-        resultElement.textContent = `Посчитал: ${result}`; 
+button.addEventListener('click', function() {
+    if (isChanged) {
+        abz.style.color = orcolor;
+        abza.style.color = orcolor;
+        abz.textContent = 'Абзац незнаю где';
+        abza.textContent = 'Абзац незнаю когда';
+        isChanged = false;
     } else {
-        resultElement.textContent = "Введи числа"; 
+        abz.style.color = 'red';
+        abza.style.color = randomColor;
+
+        abz.textContent = 'Ты кто?';
+        abza.textContent = 'Незнаю';
+        isChanged = true;
+    }
+});
+
+sbutton.addEventListener('click', function() {
+    beka.classList.toggle('hanged-style');
+    if (beka.classList.contains('hanged-style')) {
+        beka.textContent = 'Нету время';
+    } else {
+        beka.textContent = 'Кто где когда';
     }
 });
